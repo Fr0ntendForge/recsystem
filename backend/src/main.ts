c60f88ce09bddf513d2ac8d2dba423d27daf53a0
Роман Юрьевич Троицкий <vzhyx@ya.ru>
2023-05-22 03:42:52 +0300

    feat: swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,12 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 import * as dotenv from 'dotenv'; // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import { setupSwagger } from './swagger/swagger';
 
 dotenv.config();
 const port = process.env.SERVER_PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api'); // Установка префикса для всех роутов
+
+  setupSwagger(app);
+
   await app.listen(port);
 }
 
